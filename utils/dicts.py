660a5f2b705081c32fce0f711ba558dd6ff9588b
Anton Glazunov <tagl91@yandex.ru>
2024-03-07 22:58:22 +0300

    add func & tests

---
+++ utils/dicts.py
@@ -0,0 +1,17 @@
+"""Функции для работы со словорями"""
+
+def get_val(collection, key=None, default='git'):
+    """
+    Функция возвращает значение из словаря по переданному ключу,
+    если ключ существует. В ином случае возвращается значение
+    по-умолчанию.
+    :param collection: исходный словарь.
+    :param key: ключ извлекаемого элемента.
+    :param default: значение по-умолчанию.
+    :return: значение по индексу или значение по-умолчанию.
+    """
+    if key != None:
+        for key_coll in collection.keys():
+            if key_coll == key:
+                return collection[key]
+    return default
